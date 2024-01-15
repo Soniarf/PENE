555517f0b3d3cb459ffdb0ec0d59d2aac6da2e6c
Soniarf <soniarf11@educastur.es>
2024-01-15 14:15:50 +0100

    Botones Interactuables Array

--- Assets/Scripts/SistemaNiveles.cs
+++ Assets/Scripts/SistemaNiveles.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SistemaNiveles : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static SistemaNiveles instancia;
+    public Button[] botonesNiveles;
+    public int desbloaquearNiveles;
+
+
+    private void Awake()
+    {
+        if(instancia == null)
+        {
+            instancia = this;
+        }
+    }
     void Start()
     {
-        
+        if (botonesNiveles.Length > 0)
+        {
+            for (int i = 0; i < botonesNiveles.Length; i++)
+            {
+                botonesNiveles[i].interactable = false;
+            }
+
+            for (int i = 0; i < PlayerPrefs.GetInt("nivelesDesbloqueados",1); i++)
+            {
+                botonesNiveles[i].interactable = true;
+            }
+        }
     }
 
     // Update is called once per frame
