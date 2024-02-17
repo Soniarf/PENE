ef4994e33d36e9c35b114d7b2d30ba964facfed3
Sonia <soniarf11@educastur.es>
2024-02-17 22:11:31 +0100

    Orden de respuestas aleatorizado

--- Assets/Scripts/PreguntaSetUp.cs
+++ Assets/Scripts/PreguntaSetUp.cs
@@ -24,6 +24,15 @@ public class PreguntaSetUp : MonoBehaviour
         GetQuestionAssets();
     }
 
+    void Start()
+    {
+        selectNewQuestion();
+        setQuestionValues();
+        setAnswerValues();
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
@@ -33,5 +42,44 @@ public class PreguntaSetUp : MonoBehaviour
     private void GetQuestionAssets()
     {
         preguntas = new List<PreguntaData>(Resources.LoadAll<PreguntaData>("Preguntas"));
+    }
+
+
+
+    private void selectNewQuestion()
+    {
+        int randomQuestionIndex = Random.Range(0, preguntas.Count);
+        preguntaactual = preguntas[randomQuestionIndex];
+        preguntas.RemoveAt(randomQuestionIndex);
+    }
+
+
+    private void setQuestionValues()
+    {
+        textoPregunta.text = preguntaactual.pregunta;
+    }
+
+
+    private void setAnswerValues()
+    {
+        List<string> respuestas = randomizeRespuestas(new List<string>(preguntaactual.respuestas));
+
+        for (int i = 0; i < botonesRespuestas.Length; i++)
+        {
+            bool isCorrect = false;
+
+            if (i == eleccionRespuestaCorrecta)
+            {
+                isCorrect = true;
+            }
+
+            botonesRespuestas[i].SetIsCorrect(isCorrect);
+            botonesRespuestas[i].SetTextoRespuesta(respuestas[i]);
+        }
+    }
+
+    private List<string> randomizeRespuestas(List<string> originalList)
+    {
+
     }
 }
